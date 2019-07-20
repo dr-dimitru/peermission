c9366124aa61af33ce24c28010ba4015c98439d2
Vadim <topcyprus@gmail.com>
2019-07-20 14:46:18 +0300

    css

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 import '/imports/client/index/controller.js';
 import '/imports/client/chats/controller.js';
 import '/imports/client/chat/controller.js';
+import '/client/main.css';
 
 import '/imports/client/routes.js';
+
